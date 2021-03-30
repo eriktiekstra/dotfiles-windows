a48a075cddc2226d921437af8541064fa494ace2
Erik Tiekstra <erik.tiekstra@arbetsformedlingen.com>
2021-03-30 11:41:22 +0200

    Installed new zsh theme

--- install/install-oh-my-zsh.sh
+++ install/install-oh-my-zsh.sh
@@ -1,4 +1,5 @@
 [ -d ~/.oh-my-zsh ] || git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+[ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ] || git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 echo "✔ Oh-my-zsh installed and configured!"
--- install/set-zsh-as-default.sh
+++ install/set-zsh-as-default.sh
@@ -2,4 +2,5 @@ ln -sf $(pwd)/bash/bashrc ~/.bashrc
 echo "✔ ZSH set as default shell!"
 
 ln -sf $(pwd)/zsh/zshrc ~/.zshrc
+ln -sf $(pwd)/zsh/p10k.zsh ~/.p10k.zsh
 echo "✔ ZSH file symlinked!"
