263071ccb12070d7858e588350da089fd5d7774a
Erik Tiekstra <erik.tiekstra@arbetsformedlingen.com>
2021-03-30 10:13:14 +0200

    Updated nvm version

--- install/install-nvm.sh
+++ install/install-nvm.sh
@@ -1,5 +1,5 @@
 
 ln -sf $(pwd)/nvm/nvmrc ~/.nvmrc
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | zsh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | zsh
 . ~/.nvm/nvm.sh install
 echo "✔ NVM installed and configured!"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,20 +9,35 @@ alias home='cd ~'
 alias sites='cd ~/Sites'
 alias dotfiles='cd ~/.dotfiles'
 
+# # Colors
+Color_Off='\033[0m'       # Text Reset
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Cyan='\033[0;36m'         # Cyan
+Red='\033[0;31m'         # Red
+
 af() {
 	AF_SOURCE=/etc/resolv.conf-af
 	TARGET=/etc/resolv.conf
 
-	sudo rm ${TARGET}
-	sudo cp ${AF_SOURCE} ${TARGET}
+	echo -e "${Cyan}Replacing resolv.conf to be able to access AF's internal network${Color_Off}"
+	if sudo rm ${TARGET} && sudo cp ${AF_SOURCE} ${TARGET} ; then 
+		echo -e "${Green}You are now using AF's internal network${Color_Off}"
+	else 
+		echo -e "${Red}Replacing resolv.conf files failed!${Color_Off}"
+	fi
 }
 
 global() {
 	GLOBAL_SOURCE=/etc/resolv.conf-global
 	TARGET=/etc/resolv.conf
 
-	sudo rm ${TARGET}
-	sudo cp ${GLOBAL_SOURCE} ${TARGET}
+	echo -e "${Cyan}Replacing resolv.conf to be able to access the global network${Color_Off}"
+	if sudo rm ${TARGET} && sudo cp ${GLOBAL_SOURCE} ${TARGET} ; then 
+		echo -e "${Green}You are now using the global network${Color_Off}"
+	else 
+		echo -e "${Red}Replacing resolv.conf files failed!${Color_Off}"
+	fi
 }
 
 ssh-create-key() {
@@ -34,8 +49,10 @@ set-env() {
 	TARGET=/mnt/c/Windows/System32/drivers/etc/hosts
 
 	echo -e "${Cyan}Replacing hosts file...${Color_Off}"
-	rm $TARGET
-	cp $SOURCE $TARGET
-	source $TARGET 2> /dev/null
-	echo -e "${Green}You are now running the ${Yellow}${1}${Green} hosts file${Color_Off}"
+
+	if rm $TARGET && cp $SOURCE $TARGET && source $TARGET 2> /dev/null ; then
+		echo -e "${Green}You are now running the ${Yellow}${1}${Green} hosts file${Color_Off}"
+	else
+		echo -e "${Red}Replacing hosts file failed!${Color_Off}"
+	fi
 }
